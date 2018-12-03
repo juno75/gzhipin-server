53fd3e252371ec840f8d4c8575b5235105b32645
juno <18845792124@163.com>
2018-12-03 21:29:11 +0800

    初始化配置完成

---
+++ model/users.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+const usersSchema = new mongoose.Schema({
+
+})
+
+module.exports = mongoose.model('Users',usersSchema);
---
+++ router/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = new express.Router();
+
+router.get('/',(req,res)=>{
+    res.send('这是服务器返回的响应12344545')
+})
+
+module.exports = router;
